c2f744642bcca1a1b359828f8420aa74da950101
Kai Chang <hchang@tevora.com>
2019-06-20 14:34:51 -0700

    Trying to implement heap sort...

--- sorting_algorithms.cpp
+++ sorting_algorithms.cpp
@@ -3,6 +3,7 @@
 #include <random>
 #include <iterator>
 #include <iostream>
+#include <cmath>
 
 void print_vector( std::vector<int> vec )
 {
@@ -27,6 +28,7 @@ void generate_vector( std::vector<int> &vec, int size )
 	std::shuffle( vec.begin(), vec.end(), g );
 }
 		
+//A list of the sorting algorithms
 void bubble_sort( std::vector<int> &vec )
 {
 	bool swapped = false;
@@ -51,12 +53,112 @@ void bubble_sort( std::vector<int> &vec )
 		//This is to make it O(n) when sorted:
 		if( swapped == false )
 		{
-			std::cout << "no more swaps to perform, it's sorted" << std::endl;
 			return;
 		}
 	}
 }
-		
+
+//Insertion sort has a sorted partition and an unsorted partition
+void insertion_sort( std::vector<int> &vec )
+{
+	int size = vec.size();
+	int sorted_size = 1;
+
+	//Breaking if the vector is 0 or 1 elements in size
+	if( vec.size() < 2 )
+		return;
+
+	for( int i = 1; i < size; ++i )
+	{
+		int j = i;
+		while( j > 0 && vec[j] < vec[j-1] )
+		{
+			std::swap( vec[j], vec[j-1] );
+			--j;
+		}
+	}
+}
+
+//Selection sort: pick the max every time
+void selection_sort( std::vector<int> &vec )
+{
+	int max = -1;
+	int max_ind = -1;
+	for( int outer = vec.size()-1; outer > 0; --outer )
+	{
+		for( int i = 0; i <= outer; ++i )
+		{
+			if( vec[i] > max )
+			{
+				max = vec[i];
+				max_ind = i;
+			}
+		}
+		//std::cout << "new max: " << max << "("<< vec[max_ind] << ", at " << max_ind << std::endl;
+		//print_vector( vec );
+		std::swap( vec[max_ind], vec[outer] );
+		//print_vector( vec );
+		max = -1;
+		max_ind = -1;
+	}
+}
+
+
+/* DIVIDE AND CONQUEROR */
+
+
+
+
+
+
+
+
+
+
+
+
+/* TREE SORTING ALGOS */
+void heapify( int i, std::vector<int> &t )
+{
+	//case where there are not enough children
+	if( i*2 + 1 > t.size() )
+		return;
+	else
+	{
+		if( t[i] < t[i*2 + 1] )
+		{
+			std::cout << "swap " << t[i] << " & " << t[i*2+1] << std::endl;
+			std::swap( t[i], t[i*2 + 1] );
+		}
+		if( i*2 + 2 > t.size() )
+			return;
+		else if( t[i] < t[i*2 + 2] )
+		{
+			std::cout << "swap " << t[i] << " & " << t[i*2+1] << std::endl;
+			std::swap( t[i], t[i*2 + 1] );
+		}
+	}
+
+}
+
+void heapify_vector( std::vector<int> &t )
+{
+	for( int i = std::ceil( (t.size()-1) / 2 ); i >= 0; --i )
+	{
+		std::cout << "n: " << i << std::endl;
+		heapify( i, t );
+	}
+}
+
+
+
+
+
+
+
+
+
+
 
 int main()
 {
@@ -64,16 +166,28 @@ int main()
 	//std::cout << "Vector size? " << std::endl;
 	//std::cin << size;
 	std::vector<int> unsorted;
-	std::vector<int> sorted = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	//std::vector<int> sorted = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
 	generate_vector( unsorted, size );
+	
+	
+	//Unsorted tree:
+
 
 	print_vector( unsorted );
-	bubble_sort( unsorted ); //this is a dumb O(n^2) algorithm, doesn't stop if there are no swaps
+	//bubble_sort( unsorted ); //this is a dumb O(n^2) algorithm, doesn't stop if there are no swaps
+	//insertion_sort( unsorted );
+	//selection_sort( unsorted );
+	//
+	//Divide and conqueror sorts:
+	//quick_sort( unsorted );
+	//merge_sort( unsorted );
+	
+	//heap_sort( unsorted );
+	//
+	//tree_sort( unsorted );
+	heapify_vector( unsorted );
 	print_vector( unsorted );
-
-	std::cout << "testing if it breaks early" << std::endl;
-	print_vector( sorted );
-	bubble_sort( sorted );
 
 	return 0;
 }
